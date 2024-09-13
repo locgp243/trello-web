36d567f067b2c02a335af4ecb11ff42ec2ab9d4b
gialocdev <gialocfresher@gmail.com>
2024-09-13 16:44:49 +0700

    fixbug empty column

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -25,6 +25,7 @@ function Card({ card }) {
     opacity: isDragging ? 0.5 : undefined,
     border: isDragging ? '1px solid #2ecc71' : undefined
 
+
   }
 
   const shouldShowCardActions = () => {
@@ -40,7 +41,11 @@ function Card({ card }) {
       sx={{
         cursor: 'pointer',
         boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
-        overflow: 'unset'
+        overflow: 'unset',
+        display: card?.FE_PlaceholderCard ? 'none' : 'block'
+        //Cách khác nếu không dùng được display
+        //overflow: card?.FE_PlaceholderCard ? 'hidden' : 'unset',
+        //height: card?.FE_PlaceholderCard ? '0px' : 'unset',
       }}>
       {card?.cover &&
         <CardMedia sx={{ height: 140 }} image={card?.cover}/>
